cb90d910f6c192158f3cc0a39086977f39db77e0
Emanuel Pituch <Emanuel.Pituch@libertymutual.com>
2019-07-09 13:41:04 -0400

    Spelled express correctly

--- index.js
+++
@@ -1,6 +0,0 @@
-const pally = require('pa11y');
-
-pally('https://www.libertymutual.com/').then((results) => {
-    // Do something with the results
-    console.log(results);
-});
--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-const express = require('experss');
+const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const app = express();
+const pally = require('pa11y');
+
+pally('https://www.libertymutual.com/').then((results) => {
+    // Do something with the results
+    console.log(results);
+});
