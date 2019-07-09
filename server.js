832913dc814e272b7d8fca5397fad73f0683c923
Emanuel Pituch <Emanuel.Pituch@libertymutual.com>
2019-07-09 14:42:24 -0400

    Added post request

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const http = require('http');
-const bodyParser = require('body-parser');
 const app = express();
 const pally = require('pa11y');
 
-pally('https://www.libertymutual.com/').then((results) => {
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+let port = 3000;
+
+app.post('/api/score/', (req, res) =>{
+    let url = req.body.url;
+    console.log(`URL: ${url}`);
+    res.end('done');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+/*pally('https://www.libertymutual.com/').then((results) => {
     // Do something with the results
     console.log(results);
-});
+});*/
