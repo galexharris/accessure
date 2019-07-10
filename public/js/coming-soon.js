564d059a63c8ec548d7af1171308cae4a9266b9c
Max Thielmeyer <Max.Thielmeyer@libertymutual.com>
2019-07-09 22:07:23 -0400

    getting count B^)

--- public/js/coming-soon.js
+++ public/js/coming-soon.js
@@ -12,20 +12,27 @@ function sendURL() {
         success: function (data) {
             document.getElementById('loading-img').style.visibility = "hidden";
             console.log(data);
+            console.log(data[0]);
             document.getElementById("pageStyle").setAttribute("href", "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css");
             var video = document.getElementById("myvid");
             var mast = document.getElementById("mast");
             video.style.display = "none";
             mast.style.display = "none";
             var i;
-            for(i = 0; i < data.issues.length; i++){
-              document.getElementById("allContainers").style.display = "block"
-                          var listElem = document.createElement("LI");
-              listElem.innerHTML = data.issues[i].message;
-              document.getElementById("issues3").appendChild(listElem);
-            }
+            document.getElementById("allContainers").style.display = "block"
 
-            document.getElementById("num3").innerHTML = data.issues.length + " Issues";
+            // for(i = 0; i < data.issues.length; i++){
+            //               var listElem = document.createElement("LI");
+            //   listElem.innerHTML = data.issues[i].message;
+            //   document.getElementById("issues3").appendChild(listElem);
+            // }
+
+            document.getElementById("num5").innerHTML = data[0].num_errors + " Issues";
+            document.getElementById("num2").innerHTML = data[1].num_errors + " Issues";
+            document.getElementById("num1").innerHTML = data[2].num_errors + " Issues";
+            document.getElementById("num3").innerHTML = data[3].num_errors + " Issues";
+            document.getElementById("num4").innerHTML = data[4].num_errors + " Issues";
+
 
         }
     });
