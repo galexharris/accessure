595e61e6a857c20f37efeb8a793bbdd1fea958c9
Max Thielmeyer <Max.Thielmeyer@libertymutual.com>
2019-07-16 14:58:56 -0400

    changed port in cloud env

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let db = require("./db.js")
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
-let port = 3000;
+// let port = 3000;
 
 app.get('/', (req, res) =>{
     res.sendFile('index.html');
@@ -20,10 +20,15 @@ app.post('/api/score/', (req, res) =>{
     console.log(url);
     pally(url).then((results) => {
         // Do something with the results
-        
+
         res.send(db.generateReport(results.issues));
     });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+let port = process.env.VCAP_APP_PORT || 3001
+//
+// app.listen(port, () => {
+// 		console.log(`Socket & Express Server running on port ${port}`)
+// 	})
 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
